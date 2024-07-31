0524e7acbf58a84cd1557b5225eb9678ae614cc5
MAbdelmoati2025 <142133829+MAbdelmoati2025@users.noreply.github.com>
2024-07-31 04:08:55 +0300

    Delete iic2 directory

--- iic2/Debug/MCAL/DIO/DIO.d
+++
@@ -1,9 +0,0 @@
-MCAL/DIO/DIO.o MCAL/DIO/DIO.o: ../MCAL/DIO/DIO.c ../MCAL/DIO/DIO.h \
-  ../MCAL/DIO/../../UTLIS/Common_Macros.h \
-  ../MCAL/DIO/../../UTLIS/Std_Types.h
-
-../MCAL/DIO/DIO.h:
-
-../MCAL/DIO/../../UTLIS/Common_Macros.h:
-
-../MCAL/DIO/../../UTLIS/Std_Types.h:
